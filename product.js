bd48d0e4bcc1faf5a4fd6371331c8008df0eaddb
AnMarina <99315263+AnMarina@users.noreply.github.com>
2022-05-19 00:19:41 +0600

    Add files via upload

---
+++ product.js
@@ -0,0 +1,309 @@
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
+
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+let addToCartButtons = document.getElementsByClassName('btn-primary')
+let cartContainer = document.getElementsByTagName('tbody')[0]
+let quantityFields = document.getElementsByClassName('num')
+let delete_buttons = document.getElementsByClassName('uk-button-danger')
+
+// picking up all the Add-To-Cart buttons
+for(let i = 0; i < addToCartButtons.length; i++){
+    addToCartButtons[i].addEventListener('click', addToCart)
+
+}
+// This function helps to add items to our cart
+function addToCart(event){
+
+
+    let itemContainer = document.createElement('tr')
+    let btn = event.target
+    let btnGrandParent = btn.parentElement.parentElement
+    let btnParent = btn.parentElement
+    let itemImage = btnGrandParent.children[0].src
+    let itemName = btnParent.children[0].innerText
+    let itemPrice = btnParent.children[1].innerText
+
+
+    itemContainer.innerHTML = `
+    <td><input class="uk-checkbox" type="checkbox"></td>
+    <td><img class="uk-preserve-width uk-border-circle" src=${itemImage} width="40" alt=""></td>
+    <td class="uk-table-link">
+        <h3 class = "item-name">${itemName}</h3>
+    </td>
+    <td class="uk-text-truncate item-price"><h3>${itemPrice}</h3></td>
+    <td><input type = 'number' class = 'num' value = '1'></td>
+    <td class="uk-text-truncate total-price"><h3>${itemPrice}</h3></td>
+    <td><button class="uk-button uk-button-danger" type="button">Remove</button></td>
+`
+
+    cartContainer.append(itemContainer)
+
+
+
+
+    // Accessing individual quantity fields
+    for(let i = 0; i < quantityFields.length; i++){
+        quantityFields[i].value = 1
+        quantityFields[i].addEventListener('change', totalCost)
+
+    }
+
+    // Accessing individual quantity fields
+    for(let i = 0; i < delete_buttons.length; i++){
+        delete_buttons[i].addEventListener('click', removeItem)
+    }
+
+    grandTotal()
+
+
+}
+
+
+
+// This function helps to multiply the quantity and the price
+function totalCost(event){
+    let quantity = event.target
+    quantity_parent = quantity.parentElement.parentElement
+    price_field = quantity_parent.getElementsByClassName('item-price')[0]
+    total_field = quantity_parent.getElementsByClassName('total-price')[0]
+    price_field_content = price_field.innerText.replace('$', '')
+    total_field.children[0].innerText = '$' +  quantity.value * price_field_content
+    grandTotal()
+    if(isNaN(quantity.value)|| quantity.value <= 0){
+        quantity.value = 1
+    }
+
+
+
+}
+
+// This function helps to add up the total of the items
+function grandTotal(){
+    let total = 0
+    let grand_total = document.getElementsByClassName('grand-total')[0]
+    all_total_fields = document.getElementsByClassName('total-price')
+    for(let i = 0; i < all_total_fields.length; i++){
+        all_prices = Number(all_total_fields[i].innerText.replace('$', ''))
+        total+=all_prices
+    }
+    grand_total.children[0].innerText = "$"+total
+    grand_total.children[0].style.fontWeight = 'bold'
+    console.log(total)
+}
+
+
+function removeItem(event){
+    del_btn = event.target
+    del_btn_parent = del_btn.parentElement.parentElement
+    del_btn_parent.remove()
+    console.log(del_btn)
+    grandTotal()
+
+}
+
+
+const responsiveNavbar = (function () {
+	const button = document.querySelector("#menuButton");
+	const navbar = document.querySelector("#navbar")
+	button.addEventListener("click", function () {
+		if (navbar.className === "navbar") {
+			navbar.className += " navbarResponsive";
+		}
+		else {
+			navbar.className = "navbar";
+		}
+	});
+})();
+
+if (document.getElementById('hearderSlide')) {
+	$('#hearderSlide').multislider();
+	$('#hearderSlide').multislider('pause');
+}
+
+
+function closeCart() {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling')
+}
+
+
+const openShopCart = document.querySelector('.shoppingCartButton');
+openShopCart.addEventListener('click', () => {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling');
+});
+
+
+const closeShopCart = document.querySelector('#closeButton');
+const overlay = document.querySelector('.overlay');
+closeShopCart.addEventListener('click', closeCart);
+overlay.addEventListener('click', closeCart);
+
+let productsInCart = [];
+const parentElement = document.querySelector('#buyItems');
+const cartSumPrice = document.querySelector('#sum-prices');
+const products = document.querySelectorAll('.preview');
+
+
+const countTheSumPrice = function () {
+	let sum = 0;
+	productsInCart.forEach(item => {
+		sum += product.price;
+	});
+	return sum;
+}
+
+const updateShoppingCartHTML = function () {
+	if (productsInCart.length > 0) {
+		let result = productsInCart.map(product => {
+			return `
+				<li class="buyItem">
+					<img src="${product.image}">
+					<div>
+						<h5>${product.name}</h5>
+						<h6>${product.price}</h6>
+						<div>
+							<button class="button-minus" data-id=${product.id}>-</button>
+							<span class="countOfProduct">${product.count}</span>
+							<button class="button-plus" data-id=${product.id}>+</button>
+						</div>
+					</div>
+				</li>`
+		});
+		parentElement.innerHTML = result.join('');
+		document.querySelector('.checkout').classList.remove('hidden');
+		cartSumPrice.innerHTML = "KZT" + countTheSumPrice();
+
+	}
+	else {
+		document.querySelector('.checkout').classList.add('hidden');
+		parentElement.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>';
+		cartSumPrice.innerHTML = '';
+	}
+}
+
+function updateProductsInCart(product) {
+	for (let i = 0; i < productsInCart.length; i++) {
+		if (productsInCart[i].id == product.id) {
+			productsInCart[i].count += 1;
+			productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
+			return;
+		}
+	}
+	productsInCart.push(product);
+}
+
+products.forEach(product => {
+	product.addEventListener('click', (e) => {
+		if (e.target.classList.contains('cart')) {
+			const productID = e.target.dataset.productId;
+			const productName = product.querySelector('.productName').innerHTML;
+			const productPrice = product.querySelector('.price').innerHTML;
+			const productImage = product.querySelector('img').src;
+			let productToCart = {
+				name: productName,
+				image: productImage,
+				id: productID,
+				count: 1,
+				price: +productPrice,
+				basePrice: +productPrice,
+			}
+			updateProductsInCart(productToCart);
+			updateShoppingCartHTML();
+		}
+	});
+});
+
+parentElement.addEventListener('click', (e) => { // Last
+	const isPlusButton = e.target.classList.contains('button-plus');
+	const isMinusButton = e.target.classList.contains('button-minus');
+	if (isPlusButton || isMinusButton) {
+		for (let i = 0; i < productsInCart.length; i++) {
+			if (productsInCart[i].id == e.target.dataset.id) {
+				if (isPlusButton) {
+					productsInCart[i].count += 1
+				}
+				else if (isMinusButton) {
+					productsInCart[i].count -= 1
+				}
+				productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
+
+			}
+			if (productsInCart[i].count <= 0) {
+				productsInCart.splice(i, 1);
+			}
+		}
+		updateShoppingCartHTML();
+	}
+});
+
+updateShoppingCartHTML();
