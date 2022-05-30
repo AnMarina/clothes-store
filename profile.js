2d060fa259a95c94f27f74cf92783d1fffd9b3d7
AnMarina <99315263+AnMarina@users.noreply.github.com>
2022-05-31 01:42:21 +0600

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,12 @@
+const responsiveNavbar = (function () {
+	const button = document.querySelector("#menuButton");
+	const navbar = document.querySelector("#navbar")
+	button.addEventListener("click", function () {
+		if (navbar.className === "navbar") {
+			navbar.className += " navbarResponsive";
+		}
+		else {
+			navbar.className = "navbar";
+		}
+	});
+})();
